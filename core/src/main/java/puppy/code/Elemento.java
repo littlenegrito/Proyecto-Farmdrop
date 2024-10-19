c4f98e5ac7f434a89b1ade7c4f6bfc7b38efb3e8
Andres Garcia Muñoz <163463179+littlenegrito@users.noreply.github.com>
2024-10-19 19:47:16 -0300

    Creacion interfaces elemento y habilidad

---
+++ core/src/main/java/puppy/code/Elemento.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package puppy.code;
+
+/**
+ *
+ * @author Acer
+ */
+public interface Elemento {
+    int obtenerPuntaje();
+    boolean esBueno();
+    int obtenerDaño();
+    int obtenerCuracion();
+    String obtenerNombre();
+}
---
+++ core/src/main/java/puppy/code/Habilidad.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package puppy.code;
+
+/**
+ *
+ * @author Acer
+ */
+public interface Habilidad {
+    void usarHabilidad();
+    String obtenerNombre();
+    int  obtenerCooldown();
+    String obtenerCaracteristica();
+}
