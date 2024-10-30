d378bfa1761e14088d41a3685386f30a011bf0ce
Marcelo Morales <163350569+marselomorales@users.noreply.github.com>
2024-10-30 20:21:55 -0300

    Update GameLluviaMenu.java

--- core/src/main/java/puppy/code/GameLluviaMenu.java
+++ core/src/main/java/puppy/code/GameLluviaMenu.java
@@ -1,19 +1,23 @@
 package puppy.code;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 	public class GameLluviaMenu extends Game {
 
 		private SpriteBatch batch;
-		private BitmapFont font;
-		private int higherScore;
+                private BitmapFont font;
+                private TextureAtlas atlas; // Carga el atlas aquí
+                private int higherScore;
 
 		public void create() {
 			batch = new SpriteBatch();
-			font = new BitmapFont(); // use libGDX's default Arial font
-			this.setScreen(new MainMenuScreen(this));
+                        font = new BitmapFont(); // Usamos la fuente predeterminada de LibGDX
+                        atlas = new TextureAtlas(Gdx.files.internal("output/imagenesAtlas.atlas")); // acceder a las texturas generadas
+                        this.setScreen(new MainMenuScreen(this));
 		}
 
 		public void render() {
@@ -23,6 +27,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 		public void dispose() {
 			batch.dispose();
 			font.dispose();
+                        atlas.dispose();
 		}
 
 		public SpriteBatch getBatch() {
@@ -32,6 +37,10 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 		public BitmapFont getFont() {
 			return font;
 		}
+                
+                public TextureAtlas getAtlas() { 
+                    return atlas;
+                }
 
 		public int getHigherScore() {
 			return higherScore;
